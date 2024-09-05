0913ae6e09850979fcec7d5f61397edf66d577a2
Matheus <szmatheussouza@gmail.com>
2024-09-05 19:45:26 -0300

    implementando rota

--- src/index.ts
+++ src/index.ts
@@ -29,21 +29,20 @@ app.get('/', async function (request: Request, response: Response) {
     }
 });
 
-app.get('/detalhes/:name', async function (request: Request, response: Response) {
+app.get('/pokemon/:name', async function (request: Request, response: Response) {
+    const pokemonName = request.params.name;
+    
     try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1");
-        const data = await res.json();
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const pokemonData = await res.json();
 
-        // Buscando detalhes de cada Pokémon
-        const pokemonDetailsPromises = data.results.map((pokemon: { url: string }) => fetchPokemonDetails(pokemon.url));
-        const pokemonDetails = await Promise.all(pokemonDetailsPromises);
-
-        response.render("index", { results: pokemonDetails });
+        response.render("pokemonDetails", {pokemon: pokemonData});
     } catch (error) {
         console.error(error);
-        response.status(500).send("Erro ao buscar dados do Pokémon.");
+        response.status(500).send("Erro ao buscar detalhes do Pokémon");
     }
-});
+})
+
 
 app.listen(3000, function () {
     console.log("Server is running");
