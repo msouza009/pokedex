85c56ed885f1faf8586b3d24c6f668945a21cf9e
Matheus <ra-23371012-2@alunos.unicesumar.edu.br>
2024-09-09 21:42:55 -0300

    removedd comment

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
 
-// Função para buscar dados de um Pokémon
 async function fetchPokemonDetails(pokemonUrl: string) {
     const response = await fetch(pokemonUrl);
     const data = await response.json();
     return data;
 }
 
-// Rota principal que lista todos os Pokémon com detalhes adicionais
 app.get('/', async function (request: Request, response: Response) {
     try {
         const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
         const data = await res.json();
 
-        // Buscando detalhes de cada Pokémon
         const pokemonDetailsPromises = data.results.map((pokemon: { url: string }) => fetchPokemonDetails(pokemon.url));
         const pokemonDetails = await Promise.all(pokemonDetailsPromises);
 
